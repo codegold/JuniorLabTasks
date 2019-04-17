318537ba379a7e9bfee41bab212f7b6f3743e38e
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-04-17 14:55:40 +0300

    Find num that appears even times

--- JuniorLabTasks/src/FindOddIn/FindOdd.java
+++ JuniorLabTasks/src/FindOddIn/FindOdd.java
@@ -2,17 +2,18 @@ package FindOddIn;
 
 public class FindOdd {
     public static int findOdd(int[] arr) {
-        int r = 0;
-        for(int num: arr)
-            r ^=num;
-        return r;
+        int rep = 0;
+        for (int num : arr) {
+            rep ^= num;
+        } return rep;
     }
 
+
     public static void main(String[] args) {
-        int[] arr = {20,1,-1,2,-2,3,3,5,5,1,3,3,3,3,2,4,20,4,-1,-2,5};
+        int[] arr = {20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5};
         System.out.println(findOdd(arr));
         int x = 342;
-        System.out.println(Integer.toBinaryString(x));
-        System.out.println(343|342);
+//        System.out.println(Integer.toBinaryString(x));
+//        System.out.println(343 | 342);
     }
 }
