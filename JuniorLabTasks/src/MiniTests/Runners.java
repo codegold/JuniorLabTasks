02194d20c80d7d9c53d1333d18c43fb77a85da38
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-05-20 13:21:57 +0300

    Add files via upload

--- JuniorLabTasks/src/MiniTests/Runners.java
+++ JuniorLabTasks/src/MiniTests/Runners.java
@@ -12,14 +12,15 @@ public class Runners {
         int[] times = {341, 273, 278, 329, 445, 402, 388, 275, 243,
                 334, 412, 393, 299, 343, 317, 265};
 
-        int firstRun = first_runner(times);
-        int secondRun = second_runner(times);
+        int firstRun = firstRunner(times);
+        int secondRun = secondRunner(times);
         //int second = second(times);
         System.out.println(names[firstRun] + " was first!");
-        System.out.println(secondResult(times));
+        System.out.println(secondRunner(times));
+        System.out.println(secondRunner2(times));
     }
 
-    private static int first_runner(int[] times) {
+    private static int firstRunner(int[] times) {
         int num = times[0];
         int person = 0;
         for (int i = 0; i < times.length; i++) {
@@ -31,35 +32,35 @@ public class Runners {
         return person;
     }
 
-    private static int second_runner(int[] times) {
-        int num = times[0];
-        int person = 0;
-        int first = Integer.MIN_VALUE;
-        int second = Integer.MIN_VALUE;
+    private static int secondRunner(int[] times) {
+        //int num = times[0];
+
+        int firstLow = Integer.MAX_VALUE;
+        int secondLow = Integer.MAX_VALUE;
         for (int i = 0; i < times.length; i++) {
-            if (times[i] > first) {
-                second = first;
-
-                first = times[i];
-            } else if (times[i] > second)
-                second = times[i];
+            if (times[i] < firstLow) {
+                secondLow = firstLow;
+                firstLow = times[i];
+            } else if (times[i] < secondLow)
+                secondLow = times[i];
         }
-        person = second;
-        return person;
-
+        return secondLow;
     }
 
-    private static int secondResult(int[] times) {
-        int firstBest = 0, secondBest = 0;
-        for (int x : times) {
-            if (x > firstBest) {
-                secondBest = firstBest;
-                firstBest = x;
-            } else if (x > secondBest) {
-                secondBest = x;
+    private static int secondRunner2(int[] times) {
+        int first = times[times.length-1];
+        int second = times[times.length-1];
+
+        for (int i = 0; i < times.length; i++) {
+            if (times[i] < first) {
+                second = first;
+                first = times[i];
+            } else if (times[i] < second) {
+                second = times[i];
             }
-        }
-        return secondBest;
+        } return second;
+
     }
 }
 
+//Test hotkey
